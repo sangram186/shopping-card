d3e00681f2274a9b955df153cbf773675bc865c1
sangram186 <sangramnath186@gmail.com>
2020-07-24 21:27:01 +0600

    Quantity and price increase-decrease section done

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-// iphone section
 const iphonePlus = document.getElementById('iphonePlus');
 iphonePlus.addEventListener('click', function(){
-    var iphoneQuantity = document.getElementById('iphoneItem').value;
-    var iphoneQuantityNum = parseInt(iphoneQuantity);
-    var currentQuantity = iphoneQuantityNum + 1;
-    document.getElementById('iphoneItem').value = currentQuantity;
+    var prePrice = 1219;
+    shoppingPlus('iphoneItem', 'iphonePrice', prePrice);
+});
+
+const iphoneMinus = document.getElementById('iphoneMinus');
+iphoneMinus.addEventListener('click', function(){
+    var prePrice = 1219;
+    shoppingMinus('iphoneItem', 'iphonePrice', prePrice);
+});
+
+const casePlus = document.getElementById('casePlus');
+casePlus.addEventListener('click', function(){
+    var prePrice = 59;
+    shoppingPlus('caseItem', 'casePrice', prePrice);
 })
+
+const caseMinus = document.getElementById('caseMinus');
+caseMinus.addEventListener('click', function(){
+    var prePrice = 59;
+    shoppingMinus('caseItem', 'casePrice', prePrice);
+})
+
+
+
+// Making function
+function shoppingPlus(input, price, prePrice){
+    var Quantity = document.getElementById(input).value;
+    var QuantityNum = parseInt(Quantity);
+    var currentQuantity = QuantityNum + 1;
+    document.getElementById(input).value = currentQuantity;
+    var proPrice = document.getElementById(price).innerText;
+    const priceNum = parseInt(proPrice);
+    document.getElementById(price).innerText = priceNum + prePrice;
+}
+
+function shoppingMinus(input, price, prePrice){
+    var Quantity = document.getElementById(input).value;
+    var QuantityNum = parseInt(Quantity);
+    
+    if(QuantityNum >= 1){
+        var currentQuantity = QuantityNum - 1;
+        document.getElementById(input).value = currentQuantity;
+        var proPrice = document.getElementById(price).innerText;
+        var priceNum = parseInt(proPrice);
+        document.getElementById(price).innerText = priceNum - prePrice;
+    }
+}
