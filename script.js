e16487824a6e227a42cfeb7fe005d0e55633674e
sangram186 <sangramnath186@gmail.com>
2020-07-25 09:16:48 +0600

    Poorly done and all works

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var phonePrePrice = 1219;
 var casePrePrice = 59;
+
+const removeItem1 = document.getElementById('removeItem1');
+removeItem1.addEventListener('click', function(){
+    removeItem('iphoneItem', 'iphonePrice');
+});
+
+const removeItem2 = document.getElementById('removeItem2');
+removeItem2.addEventListener('click', function(){
+    removeItem('caseItem', 'casePrice');
+});
+
+function removeItem(quantity, price){
+    document.getElementById(quantity).value = 0;
+    document.getElementById(price).innerText = 0;
+    subTotalCalc()
+    taxCalc();
+    totalCalc();
+}
+
+
 const iphonePlus = document.getElementById('iphonePlus');
 iphonePlus.addEventListener('click', function(){
     shoppingPlus('iphoneItem', 'iphonePrice', phonePrePrice);
@@ -50,7 +70,7 @@ function shoppingMinus(input, price, prePrice){
     var Quantity = document.getElementById(input).value;
     var QuantityNum = parseInt(Quantity);
     
-    if(QuantityNum >= 1){
+    if(QuantityNum > 1){
         var currentQuantity = QuantityNum - 1;
         document.getElementById(input).value = currentQuantity;
 
@@ -68,7 +88,7 @@ function subTotalCalc(){
     var casePriceNum = parseInt(casePrice);
     var sum = phonePriceNum + casePriceNum;
 
-    document.getElementById('subTotal').innerText = sum;
+    document.getElementById('subTotal').innerText = formatNumber(sum);
     return sum;
 }
 
@@ -76,7 +96,7 @@ function taxCalc(){
     var subTotalNum = subTotalCalc();
     var tax = Math.round((subTotalNum / 100) * 3); // If its ok to use 3 percent tax.
 
-    document.getElementById('tax').innerText = tax;
+    document.getElementById('tax').innerText = formatNumber(tax);
     return tax;
 }
 taxCalc();
@@ -85,8 +105,33 @@ function totalCalc() {
     var subTotalAmount = subTotalCalc();
     var taxAmount = taxCalc();
     var total = subTotalAmount + taxAmount;
-    document.getElementById('total').innerText = total;
+    document.getElementById('total').innerText = formatNumber(total);
 }
 totalCalc();
 
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
 
+const input1 = document.getElementById('iphoneItem');
+const price1 = document.getElementById('iphonePrice');
+
+const input2 = document.getElementById('caseItem');
+const price2 = document.getElementById('casePrice');
+
+input1.addEventListener('input', updateValue);
+
+function updateValue(e){
+    price1.textContent = e.target.value * phonePrePrice;
+    subTotalCalc()
+    taxCalc();
+    totalCalc();
+}
+input2.addEventListener('input', updateValue);
+
+function updateValue(e){
+    price2.textContent = e.target.value * casePrePrice;
+    subTotalCalc()
+    taxCalc();
+    totalCalc();
+}
